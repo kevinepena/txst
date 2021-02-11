d342e2d8e8c7b206e3d299a4e9df9bb510eb01a0
kevinpena <kep85@txstate.edu>
2021-02-10 18:07:49 -0600

    return only 4 in row, not more than 4 & formatting

--- __test__/txst-assign.test.js
+++ __test__/txst-assign.test.js
@@ -1,43 +1,52 @@
-const { createSquareBoard, placeRandomChecker, getColumn, getDiagonally, check, checkBoard } = require('../txst-assign');
+const {
+  createSquareBoard,
+  placeRandomChecker,
+  getColumn,
+  getDiagonally,
+  check,
+  checkBoard
+} = require('../txst-assign')
 
-const sideLength = 8;
-const checkerBoard = createSquareBoard(sideLength);
+const sideLength = 8
+const checkerBoard = createSquareBoard(sideLength)
 
 describe('createBoard', () => {
-    it('reders 8 x 8 grid', () => {
-        expect(checkerBoard).toHaveLength(sideLength);
-        expect(checkerBoard.map(col => col)).toHaveLength(sideLength);
-    });
-});
+  it('reders 8 x 8 grid', () => {
+    expect(checkerBoard).toHaveLength(sideLength)
+    expect(checkerBoard.map(col => col)).toHaveLength(sideLength)
+  })
+})
 
 describe('placeRandomChecker', () => {
-    it('returns random red, black or blank', () => {
-        const possibleCheckers = ['r', 'b', ' '];
-        expect(possibleCheckers).toContainEqual(placeRandomChecker())
-    });
-});
+  it('returns random red, black or blank', () => {
+    const possibleCheckers = ['r', 'b', ' ']
+    expect(possibleCheckers).toContainEqual(placeRandomChecker())
+  })
+})
 
 describe('getColum', () => {
-    it('returns column that equals length', () => {
-        checkerBoard.map((row, i) => expect(getColumn(checkerBoard, i)).toHaveLength(sideLength))
-    });
-});
+  it('returns column that equals length', () => {
+    checkerBoard.map((row, i) =>
+      expect(getColumn(checkerBoard, i)).toHaveLength(sideLength)
+    )
+  })
+})
 
 describe('getDiagonally', () => {
-    it('return int >= 0', () => {
-        expect(getDiagonally(checkerBoard)).toBeGreaterThanOrEqual(0);
-        expect(getDiagonally(checkerBoard, true)).toBeGreaterThanOrEqual(0);
-    });
-});
+  it('return int >= 0', () => {
+    expect(getDiagonally(checkerBoard)).toBeGreaterThanOrEqual(0)
+    expect(getDiagonally(checkerBoard, true)).toBeGreaterThanOrEqual(0)
+  })
+})
 
 describe('check', () => {
-    it('returns int >= 0', () => {
-        checkerBoard.map(row => expect(check(row)).toBeGreaterThanOrEqual(0))
-    });
-});
+  it('returns int >= 0', () => {
+    checkerBoard.map(row => expect(check(row)).toBeGreaterThanOrEqual(0))
+  })
+})
 
 describe('checkBoard', () => {
-    it('returns int >= 0', () => {
-        expect(checkBoard(checkerBoard)).toBeGreaterThanOrEqual(0);
-    })
+  it('returns int >= 0', () => {
+    expect(checkBoard(checkerBoard)).toBeGreaterThanOrEqual(0)
+  })
 })
--- txst-assign.js
+++ txst-assign.js
@@ -1,81 +1,89 @@
-"use strict";
+'use strict'
 
-function createSquareBoard(sideLength) {
-    let checkerboard = [];
+function createSquareBoard (sideLength) {
+  const checkerboard = []
+  for (let i = 0; i < sideLength; i++) {
+    const row = []
     for (let i = 0; i < sideLength; i++) {
-        let row = [];
-        for (let i = 0; i < sideLength; i++) {
-            row.push(placeRandomChecker());
-        }
-        checkerboard.push(row)
+      row.push(placeRandomChecker())
     }
-    return checkerboard;
+    checkerboard.push(row)
+  }
+  return checkerboard
 }
 
-function placeRandomChecker() {
-    const checker = ["r", "b", " "];
-    return checker[Math.floor(Math.random() * checker.length)];
+function placeRandomChecker () {
+  const checker = ['r', 'b', ' ']
+  return checker[Math.floor(Math.random() * checker.length)]
 }
 
-function getColumn(board, col) {
-    return board.map(value => value[col]);
+function getColumn (board, col) {
+  return board.map(value => value[col])
 }
 
-function getDiagonally(board, up = false) {
-    const checkerboard = board;
-    const length = checkerboard.length;
-    // let temp;
-    let total = [];
-    for (let i = 0; i <= 2 * (length - 1); ++i) {
-        let temp = [];
-        for (let j = length - 1; j >= 0; --j) {
-            let x = up ? i - (length - j) : i - j;
-            (x >= 0 && x < length) && temp.push(checkerboard[j][x]);
-        }
-        total.push(check(temp));
+function getDiagonally (board, up = false) {
+  const checkerboard = board
+  const length = checkerboard.length
+  const total = []
+  for (let i = 0; i <= 2 * (length - 1); ++i) {
+    const temp = []
+    for (let j = length - 1; j >= 0; --j) {
+      const x = up ? i - (length - j) : i - j
+      x >= 0 && x < length && temp.push(checkerboard[j][x])
     }
-    return total.reduce((prev, current) => current + prev, 0);
+    total.push(check(temp))
+  }
+  return total.reduce((prev, current) => current + prev, 0)
 }
 
-function check(arr) {
-    let total = 0;
-    const amt = arr.map((checker, i, arr) => {
-        // if spot is empty - return total
-        if (checker === ' ') return total;
-        // if current checker matches previous checker
-        // add 1 to total, return
-        // else
-        // total = 0
-        return checker === arr[i - 1] ? total = total + 1 : total = total - total;
-    });
-
-    // if there is 4 and not 5
-    if (amt.includes(3) && !amt.includes(4)) return 1;
-    // if there is 8 equal in a row
-    else if (amt.includes(7)) return 2;
+function check (arr) {
+  let total = 0
+  const amt = arr.map((checker, i, arr) => {
+    // if spot is empty - return total
+    if (checker === ' ') return total
+    // if current checker matches previous checker
+    // add 1 to total, return
     // else
-    else return 0;
+    // total = 0
+    return checker === arr[i - 1]
+      ? (total = total + 1)
+      : (total = total - total)
+  })
 
+  // if there is 4 and only 4 in a row
+  if (amt.includes(3) && !amt.includes(4)) return 1
+  else return 0
 }
 
-function checkBoard(board) {
-    const checkerboard = board;
+function checkBoard (board) {
+  const checkerboard = board
 
-    const diagonalDownTotal = getDiagonally(checkerboard);
-    const diagonalUpTotal = getDiagonally(checkerboard, true);
+  const diagonalDownTotal = getDiagonally(checkerboard)
+  const diagonalUpTotal = getDiagonally(checkerboard, true)
 
-    const total = checkerboard.map((row, i) => {
-        const columnTotal = check(getColumn(checkerboard, i));
-        const rowTotal = check(row);
-        return columnTotal + rowTotal;
-    })
-    return total.reduce((prev, current) => current + prev, 0) + diagonalDownTotal + diagonalUpTotal;
+  const total = checkerboard.map((row, i) => {
+    const columnTotal = check(getColumn(checkerboard, i))
+    const rowTotal = check(row)
+    return columnTotal + rowTotal
+  })
+  return (
+    total.reduce((prev, current) => current + prev, 0) +
+    diagonalDownTotal +
+    diagonalUpTotal
+  )
 }
 
-const filledBoard = createSquareBoard(8);
-const connect4total = checkBoard(filledBoard);
+const filledBoard = createSquareBoard(8)
+const connect4total = checkBoard(filledBoard)
 
-console.log(filledBoard);
-console.log('4 in a row:', connect4total);
+console.log(filledBoard)
+console.log('4 in a row:', connect4total)
 
-module.exports = { createSquareBoard, placeRandomChecker, getColumn, getDiagonally, check, checkBoard };
+module.exports = {
+  createSquareBoard,
+  placeRandomChecker,
+  getColumn,
+  getDiagonally,
+  check,
+  checkBoard
+}
